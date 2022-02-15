c794bca64caf790c30038d58caa188373527ca87
Katie Greenwald <frostypinekng@yahoo.com>
2022-02-15 09:53:13 -0700

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import arrayimg from './pics/arrayimg.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header">hi
+<img src={arrayimg} />
       </header>
     </div>
   );
