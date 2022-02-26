ab9563e34db4c0d2a38aa92a1d430920c905813f
Katie Greenwald <frostypinekng@yahoo.com>
2022-02-25 22:11:06 -0700

    working on store and slice

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-//import {Provider} from 'react-redux';
-//import {store} from './state/store/store.js'; 
-// <Provider store ={store}></Provider>
+import {Provider} from 'react-redux';
+import store from './state/store/store'; 
+
 
 ReactDOM.render(
   <React.StrictMode>
-
+ <Provider store ={store}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-   
+   </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ client/src/state/usersSlice.js
@@ -0,0 +1,98 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { updateUser } from "../api/userAPI";
+
+let users = [];
+
+let localUserInfo = JSON.parse(localStorage.getItem("localUserInfo"));
+
+if (!localUserInfo) {
+  localUserInfo = {
+    loggedIn: false,
+  };
+  localStorage.setItem("localUserInfo", JSON.stringify(localUserInfo));
+}
+
+export const usersSlice = createSlice({
+  name: "users",
+  initialState: {
+    users,
+    localUserInfo,
+  },
+  reducers: {
+    allUsers: (state, action) => {
+      state.users = action.payload;
+    },
+    loginUser: (state, action) => {
+      state.localUserInfo = action.payload;
+      localStorage.setItem(
+        "localUserInfo",
+        JSON.stringify(state.localUserInfo)
+      );
+    },
+    addRemoteUser: (state, action) => {
+
+      // state.users.push(action.payload)
+    },
+    addUser: (state, action) => {
+      state.users.push(action.payload);
+      if (action.payload.password === "") {
+        state.localUserInfo = { user_id: action.payload._id, loggedIn: false };
+      }
+      if (action.payload.password !== "") {
+        state.localUserInfo = {
+          user_id: action.payload._id,
+          loggedIn: true,
+        };
+      }
+      localStorage.setItem(
+        "localUserInfo",
+        JSON.stringify(state.localUserInfo)
+      );
+    },
+    editUser: (state, action) => { },
+    removeUser: (state, action) => {
+      state.users.splice(
+        state.users.findIndex((e) => e._id === action.payload, 1)
+      );
+      localStorage.setItem("users", JSON.stringify(state.users));
+    },
+    editUserActive: (state, action) => {
+      const index = state.users.findIndex(
+        (user) => user._id === action.payload._id
+      );
+      state.users[index].active = action.payload.active;
+      state.users[index].socket_id = action.payload.socket_id;
+    },
+    editDisconnect: (state, action) => {
+      const index = state.users.findIndex(
+        (user) => user.socket_id === action.payload
+      );
+      state.users[index].active = false;
+      state.users[index].socket_id = "";
+      updateUser(state.users[index]._id, { socket_id: "", active: false })
+    },
+    editUserAvatar: (state, action) => {
+      const index = state.users.findIndex(
+        (user) => user._id === action.payload._id
+      );
+      state.users[index].avatar = action.payload.avatar;
+    },
+    },
+});
+
+export const {
+  allUsers,
+  loginUser,
+  addRemoteUser,
+  addUser,
+  editUser,
+  removeUser,
+  editUserActive,
+  editDisconnect,
+  editUserAvatar,
+} = usersSlice.actions;
+
+export const selectUsers = (state) => state.users.users;
+export const selectLocalUserInfo = (state) => state.users.localUserInfo;
+
+export default usersSlice.reducer;
