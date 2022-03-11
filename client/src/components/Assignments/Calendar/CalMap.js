5ea6eea139e59b72d3bbc13e5330d87385073e78
Katie Greenwald <frostypinekng@yahoo.com>
2022-03-11 10:51:56 -0700

    fix

--- client/src/components/Assignments/Calendar/CalMap.js
+++ client/src/components/Assignments/Calendar/CalMap.js
@@ -4,7 +4,7 @@
 //Katie Greenwald
 const calmap = new Map([
     ["Tue Mar 08 2022", "working day"],
-    ["Fri Mar 11 2022", "presentation"]
+    ["Fri Mar 11 2022", "presentation day"]
   ])
 
   export default calmap; 
